24b38964964056ecc7e97dade4dea860d8bf6631
Shubh-Raj <shubhraj625@gmail.com>
2025-05-16 12:57:51 +0530

    added useActionState for creating a snippet

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,12 +3,12 @@
 import { prisma } from "@/lib/prisma"
 import { redirect } from "next/navigation";
 
-export const saveSnippet = async (id:number, code:string) => {
+export const saveSnippet = async (id: number, code: string) => {
     await prisma.snippet.update({
-        where:{
+        where: {
             id
         },
-        data:{
+        data: {
             code
         }
     });
@@ -17,7 +17,29 @@ export const saveSnippet = async (id:number, code:string) => {
 
 export const deleteSnippet = async (id: number) => {
     await prisma.snippet.delete({
-        where: {id}
+        where: { id }
     });
+    redirect("/");
+}
+
+export async function createSnippet(prevState: { message: string }, formData: FormData) {
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+        return { message: "Title is required and must be longer" }
+    }
+
+    if (typeof code !== "string" || code.length < 5) {
+        return { message: "Code is required and must be longer" }
+    }
+
+    const snippet = await prisma.snippet.create({
+        data: {
+            title,
+            code
+        }
+    });
+    console.log("created snippet: ", snippet); //server side prints in console
     redirect("/");
 }
